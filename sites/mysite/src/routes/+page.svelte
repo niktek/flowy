f913f7fbe2088e4991acbdae27d881bd5a0198fc
Dominik Röschke <dominik.roeschke@web.de>
2023-08-20 10:56:08 +0200

    Modified root page

--- sites/mysite/src/routes/+page.svelte
+++ sites/mysite/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>And now we update this some more</h1>
-<p>Visit <a href="https://skeleton.dev">skeleton.dev</a> to read the documentation</p>
+<h1>Hey Nik</h1>
+<p>Wanna collaborate on dominiktek.io? ;)</p>
+<p>With love,</p>
+<p>Your German Colleague</p>
