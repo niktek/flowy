e4a82e995373cfa1e71c55e74b2326638c0dbd7a
niktek <nik@niktek.io>
2023-08-23 02:13:06 +0000

    Update path for unlinking

--- sites/mysite/scripts/prebuild.js
+++ sites/mysite/scripts/prebuild.js
@@ -2,7 +2,11 @@
 // CF_PAGES_BRANCH
 import process from 'node:process';
 import { unlinkSync } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { resolve } from 'node:path';
 
-if (!!process.env.CF_PAGES && process.env.CF_PAGES_BRANCH == 'trunk' ) {
-	unlinkSync('../static/robots.txt')
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+if (!!process.env.CF_PAGES && process.env.CF_PAGES_BRANCH == 'trunk') {
+	unlinkSync(resolve(__dirname, '../static/robots.txt'));
 }
