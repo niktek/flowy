02373001616610b1a57097743a5e2b6fbd30d72a
niktek <nik@niktek.io>
2023-08-23 02:23:18 +0000

    Update robots

--- sites/mysite/scripts/prebuild.js
+++ sites/mysite/scripts/prebuild.js
@@ -1,12 +1,16 @@
 // CF_PAGES_URL
 // CF_PAGES_BRANCH
 import process from 'node:process';
-import { unlinkSync } from 'node:fs';
+import { unlinkSync, writeFileSync } from 'node:fs';
 import { fileURLToPath } from 'url';
 import { resolve } from 'node:path';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 if (!!process.env.CF_PAGES && process.env.CF_PAGES_BRANCH == 'trunk') {
-	unlinkSync(resolve(__dirname, '../static/robots.txt'));
+	const robotsFile = resolve(__dirname, '../static/robots.txt')
+	writeFileSync(robotsFile,
+`User-agent: * 
+Disallow:
+`)
 }
