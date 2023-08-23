4f101b955a60052c55e905a77dbbda542e0c92f4
niktek <nik@niktek.io>
2023-08-23 01:56:15 +0000

    Added robot text and prebuild

---
+++ sites/mysite/scripts/prebuild.js
@@ -0,0 +1,8 @@
+// CF_PAGES_URL
+// CF_PAGES_BRANCH
+import process from 'node:process';
+
+console.log('==============================')
+console.log(process.env.CF_PAGES_URL)
+console.log(process.env.CF_PAGES_BRANCH)
+console.log('==============================')
